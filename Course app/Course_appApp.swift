4457a0e98f9fb844976eb21dfac5a6fd1012082f
mattcat1988 <mattcat12@gmail.com>
2025-02-27 09:39:03 +0700

    Initial Commit

---
+++ Course app/Course_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_appApp.swift
+//  Course app
+//
+//  Created by 1234 on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Course_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Course appTests/Course_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Course_appTests.swift
+//  Course appTests
+//
+//  Created by 1234 on 26.02.2025.
+//
+
+import Testing
+@testable import Course_app
+
+struct Course_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
